20991546a808a6333c12d0f140d4bd8fff275b68
zoykhan <32883901+zoykhan@users.noreply.github.com>
2021-06-07 12:15:11 -0500

    Update test_eem.py

--- eem/test_eem.py
+++ eem/test_eem.py
@@ -1,28 +1,23 @@
 """
-This script is trigged when a process restart syslog is found and it prints which process is restarted
+If a user restarts any process a syslog is generated. When syslog with "PROC_RESTART_NAME" is found the event script is triggered.
+The script processes the event (syslog) and prints the name of the process restarted by the user.
 
 Required configuration:
-username eem_user1
-group root-lr
-group cisco-support
-!
-aaa authorization exec default group tacacs+ local
-aaa authorization eventmanager default local
-aaa authentication login default group tacacs+ local
+User and AAA configuration
 
-event manager event-trigger trigger1
+event manager event-trigger <trigger-name>
 type syslog pattern "PROC_RESTART_NAME"
 
-event manager action action1
-username eem_user1
-type script script-name test_eem.py maxrun seconds 60 checksum disable
+event manager action <action-name>
+username <user>
+type script script-name <script-name> checksum <checksum>
 
 event manager policy-map policy1
-trigger event trigger1
-action action1
+trigger event <trigger-name>
+action <action-name>
 
 To verify:
-Check for syslog EVENT SCRIPT EXECUTED: User restarted <process_name>
+Check for syslog EVENT SCRIPT EXECUTED: User restarted <process-name>
 
 """
 from cisco.script_mgmt import xrlog
@@ -30,7 +25,10 @@ from iosxr import eem
 import re
 
 syslog = xrlog.getSysLogger("test_eem")
+
 rc, event_dict = eem.event_reqinfo()
+# event_dict consists of details of the syslog
+
 msg = event_dict['msg']
 p=re.compile("process\s(\w+[a-zA-Z])")
 process_name = p.search(msg).group()
