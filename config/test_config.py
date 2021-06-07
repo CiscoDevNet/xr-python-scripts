9a2556fa307898f5dd75f94c4123243151cca655
zoykhan <32883901+zoykhan@users.noreply.github.com>
2021-06-07 12:41:35 -0500

    Update test_config.py

--- config/test_config.py
+++ config/test_config.py
@@ -1,21 +1,32 @@
+"""
+This script validates the user configuration on a loopback interface.
+It checks for IP address to be in a certain range else shows an error and checks for netmask, if netmask does not match the validation the script changes it.
+
+"""
+
 import cisco.config_validation as xr
 import ipaddress
 
 from cisco.script_mgmt import xrlog
-logger = xrlog.getScriptLogger('xr_cli_config')
 syslog = xrlog.getSysLogger('xr_cli_config')
 
+loopback = "Loopback21041989"
+network = "100.0.0.0/8"
+mask = "255.255.255.255"
+
 def check_loopback(root):
-    int_config = root.get_node("/ifmgr-cfg:interface-configurations/interface-configuration[active='act',interface-name='Loopback21041989']")
+    int_config = root.get_node("/ifmgr-cfg:interface-configurations/interface-configuration[active='act',interface-name=loopback]")
     if int_config:
         ip_address = int_config.get_node("ipv4-io-cfg:ipv4-network/addresses/primary/address")
         syslog.info("ipaddress is " + ip_address.value)
-        syslog.info(str(ipaddress.ip_address(ip_address.value) not in ipaddress.ip_network("100.0.0.0/8")))
-        if ipaddress.ip_address(ip_address.value) not in ipaddress.ip_network("100.0.0.0/8"): 
-            xr.add_error(ip_address,"Invalid ip address, please add in range 100.0.0.0")  
+        syslog.info(str(ipaddress.ip_address(ip_address.value) not in ipaddress.ip_network(network)))
+        if ipaddress.ip_address(ip_address.value) not in ipaddress.ip_network(network): 
+            xr.add_error(ip_address,"Invalid ip address, please add in range "+ network)  
         netmask = int_config.get_node("ipv4-io-cfg:ipv4-network/addresses/primary/netmask")
         syslog.info(netmask.value)
-        if netmask.value != "255.255.255.255":
-            netmask.set_node(None,"255.255.255.255")
+        if netmask.value != mask:
+            netmask.set_node(None,mask)
+            
+            
 xr.register_validate_callback(["/ifmgr-cfg:interface-configurations/interface-configuration/*"],check_loopback)
 
