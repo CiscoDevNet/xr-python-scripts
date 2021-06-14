d75c21f1c9cc422bbe0d66c32d383463b51ad340
Zoya <zoykhan@cisco.com>
2021-06-14 11:36:57 -0700

    process script

--- process/test_process.py
+++ process/test_process.py
@@ -7,9 +7,10 @@ import time
 import os
 import xmltodict
 import re
+import argparse
 
 from cisco.script_mgmt import xrlog
-from iosxr.netconf.netconf_lib import *
+from iosxr.netconf.netconf_lib import NetconfClient
 
 log = xrlog.getScriptLogger('Sample')
 syslog = xrlog.getSysLogger('Sample')
@@ -18,26 +19,25 @@ def cpu_memory_check():
     """
     Check total routes in router
     """
-    '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument("threshold", help="cpu utilization threshold",type=int)
+    args = parser.parse_args()
+    threshold = args.threshold
     filter_string = """
     <system-monitoring xmlns="http://cisco.com/ns/yang/Cisco-IOS-XR-wdsysmon-fd-oper">
-     <cpu-utilization/>
-    </system-monitoring>"""            
+      <cpu-utilization>
+        <node-name>0/RP0/CPU0</node-name>
+          <total-cpu-one-minute/>
+      </cpu-utilization>
+    </system-monitoring>"""
     nc = NetconfClient(debug=True)
     nc.connect()
-    netconf_test_common.do_get(nc, filter=filter_string)
+    do_get(nc, filter=filter_string)
     ret_dict = _xml_to_dict(nc.reply, 'system-monitoring')
-    print(ret_dict)
+    total_cpu = int(ret_dict['system-monitoring']['cpu-utilization']['total-cpu-one-minute'])
+    if total_cpu >= threshold:
+        syslog.error("CPU utilization is %s, threshold value is %s" %(str(total_cpu),str(threshold)))
     nc.close()
-    netconf_test_common.logf.close()
-    '''
-    cpu_utilization = [78,60,80,95]
-    for cpu_util in cpu_utilization:
-        if cpu_util >= 75:
-            syslog.error("CPU utilization over 75%")
-        else:
-            syslog.info("CPU utilization normal")
-        time.sleep(5)    
     
 def _xml_to_dict(xml_output, xml_tag=None):
     """
@@ -61,14 +61,12 @@ def do_get(nc, filter=None, path=None):
         elif filter is not None:
             nc.rpc.get(request=filter)
         else:
-            print_log("ERROR: Get data is empty!\n")
             return False
     except Exception as e:
-        print_log(str(e) + "\n")
-        print_log(traceback.format_exc())
-        print_log("Caught an Exception when performing get\n")
         return False
     return True
 
 if __name__ == '__main__':
-    cpu_memory_check()
+    while(1):
+        cpu_memory_check()
+        time.sleep(30)
