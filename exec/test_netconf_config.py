7aa52e2d74027d89d3970e0dfbd81f5dc6d1e568
zoykhan <32883901+zoykhan@users.noreply.github.com>
2021-08-11 21:54:06 -0500

    Update test_netconf_config.py

--- exec/test_netconf_config.py
+++ exec/test_netconf_config.py
@@ -59,8 +59,10 @@ def config_interface():
     edit_config = """
     <interface-configurations xmlns="http://cisco.com/ns/yang/Cisco-IOS-XR-ifmgr-cfg">
       <interface-configuration>
-        <interface-name>%s</interface-name>
-      </interface-configuration>
+        <active>act</active>
+          <interface-name>%s</interface-name>
+          <interface-virtual/>
+        </interface-configuration>
     </interface-configurations>
     """ % loopback
 
